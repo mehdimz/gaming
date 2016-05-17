fe77ba964a1257b5683ce9a41569c95479c73cb5
Amir Hoseinian <amir.anyware@gmail.com>
2016-05-17 17:59:29 +0430

    add compression

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var compression = require('compression');
 var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
@@ -11,6 +12,7 @@ var flash = require('connect-flash');
 
 
 var app = express();
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'lib/storage')));
