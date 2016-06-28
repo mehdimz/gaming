77db85319684927f23dcfb3e50838f508f8706f9
Amir Hoseinian <amir.anyware@gmail.com>
2016-06-28 18:32:53 +0430

    multiple qualities added

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,7 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+  
 module.exports = function () {
   return gulp.src('lib/front/style/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
