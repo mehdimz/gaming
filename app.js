26d034e7c5a84cf4904d8eedfa0f2e321108838f
Amir Hoseinian <amir.anyware@gmail.com>
2016-08-25 15:18:02 +0430

    error page hidden message added

--- app.js
+++ app.js
@@ -79,8 +79,9 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
-    error: {}
+    message: 'صفحه مورد نظر یافت نشد',
+    hiddenMessage: err.message,
+    // error: {}
   });
 });
 
